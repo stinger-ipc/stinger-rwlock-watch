3495ad87a6699a96b776c7d8c8d5cd0a9293b3c5
Jacob Brunson <jacob.brunson@vivint.com>
2025-11-10 16:37:14 -0700

    0.3.0 Require response to write request lock

--- src/writerequest.rs
+++ src/writerequest.rs
@@ -8,54 +8,45 @@ mod tests {
     async fn test_write_request_sends_value() {
         let lock = crate::RwLockWatch::new(0);
         let mut rx = lock.take_request_receiver().expect("receiver available");
-        let request_view = lock.write_request();
+        tokio::spawn(async move {
+            while let Some((value, responder)) = rx.recv().await {
+                assert_eq!(value, 42);
+                let _ = responder.send(Some(43));
+            }
+        });
 
+        let request_view = lock.write_request();
         {
             let mut req = request_view.write().await;
             *req = 42;
+            req.send(std::time::Duration::from_secs(1)).await;
+            assert_eq!(*req, 43);
         }
-
-        let received = rx.recv().await.expect("should receive value");
-        assert_eq!(received, 42);
     }
 
     #[tokio::test]
     async fn test_multiple_requests() {
         let lock = crate::RwLockWatch::new(0);
         let mut rx = lock.take_request_receiver().expect("receiver available");
+        tokio::spawn(async move {
+            while let Some((value, responder)) = rx.recv().await {
+                let _ = responder.send(Some(10+value));
+            }
+        });
+
         let request_view = lock.write_request();
 
         for i in 1..=3 {
             let mut req = request_view.write().await;
             *req = i * 10;
+            req.send(std::time::Duration::from_secs(1)).await;
+            assert_eq!(*req, (i * 10) + 10);
         }
-
-        let mut results = vec![];
-        for _ in 0..3 {
-            results.push(rx.recv().await.unwrap());
-        }
-        assert_eq!(results, vec![10, 20, 30]);
-    }
-
-    #[tokio::test]
-    async fn test_receiver_only_once() {
-        let lock = crate::RwLockWatch::new(0);
-        let _rx = lock.take_request_receiver().expect("receiver available");
-        assert!(lock.take_request_receiver().is_none(), "second take should return None");
-    }
-
-    #[tokio::test]
-    async fn test_drop_closes_channel() {
-        let lock = crate::RwLockWatch::new(0);
-        let mut rx = lock.take_request_receiver().expect("receiver available");
-        drop(lock);
-        let result = tokio::time::timeout(Duration::from_millis(100), rx.recv()).await;
-        assert!(result.is_err() || result.unwrap().is_none(), "channel should be closed");
     }
 }
 use std::ops::{Deref, DerefMut};
 use std::sync::Arc;
-use tokio::sync::{RwLock as TokioRwLock, RwLockReadGuard, RwLockWriteGuard, TryLockError, mpsc, watch};
+use tokio::sync::{RwLock as TokioRwLock, RwLockReadGuard, RwLockWriteGuard, TryLockError, mpsc, watch, oneshot};
 
 /// A write-request view of an RwLockWatch.  Changes to the guarded value don't persist,
 /// but instead creates a request to change the value via the original lock.
@@ -63,7 +54,7 @@ use tokio::sync::{RwLock as TokioRwLock, RwLockReadGuard, RwLockWriteGuard, TryL
 pub struct WriteRequestLockWatch<T: Clone> {
     pub(crate) inner: Arc<TokioRwLock<T>>,
     pub(crate) rx: watch::Receiver<T>,
-    pub(crate) request_tx: mpsc::Sender<T>,
+    pub(crate) request_tx: mpsc::Sender<(T, oneshot::Sender<Option<T>>)>,
 }
 
 
@@ -94,7 +85,8 @@ impl<T: Clone> WriteRequestLockWatch<T> {
         let value = (*guard).clone();
         WriteRequestGuard {
             guard, // keep the original guard to block others.
-            value,
+            value: value.clone(),
+            original_value: value,
             tx: &self.request_tx,
         }
     }
@@ -105,15 +97,8 @@ pub struct WriteRequestGuard<'a, T: Clone> {
     #[allow(unused)]
     guard: RwLockWriteGuard<'a, T>,
     value: T,
-    tx: &'a mpsc::Sender<T>,
-}
-
-
-impl<'a, T: Clone> Drop for WriteRequestGuard<'a, T> {
-    fn drop(&mut self) {
-        // Try to send the requested value without waiting (we're in Drop, cannot .await)
-        let _ = self.tx.try_send(self.value.clone());
-    }
+    original_value: T,
+    tx: &'a mpsc::Sender<(T, oneshot::Sender<Option<T>>)>,
 }
 
 impl<'a, T: Clone> Deref for WriteRequestGuard<'a, T> {
@@ -127,5 +112,22 @@ impl<'a, T: Clone> Deref for WriteRequestGuard<'a, T> {
 impl<'a, T: Clone> DerefMut for WriteRequestGuard<'a, T> {
     fn deref_mut(&mut self) -> &mut Self::Target {
         &mut self.value
+    }
+}
+
+impl<'a, T: Clone> WriteRequestGuard<'a, T> {
+    /// Sends the requested value change to the request channel.
+    /// The value is set to the response from the channel, or reverts to the original value on timeout or error.
+    pub async fn send(&mut self, timeout: std::time::Duration) {
+        let (tx, rx) = oneshot::channel();
+        let _ = self.tx.send((self.value.clone(), tx)).await;
+        match tokio::time::timeout(timeout, rx).await {
+            Ok(Ok(Some(value))) => {
+                self.value = value;
+            }
+            _ => {
+                self.value = self.original_value.clone();
+            }
+        }
     }
 }
